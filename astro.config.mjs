925101b314f9d64fd205b91cfcabce1abc44f3e7
Nicolás Arce <narce@croquisweb.cl>
2025-04-02 14:17:39 -0300

    feat: add GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,8 @@ export default defineConfig({
         tailwind(),
         partytown({
             config: {
-              forward: ["dataLayer.push"],
+                forward: ["dataLayer.push"],
+                debug: false,
             },
         }),
     ],
